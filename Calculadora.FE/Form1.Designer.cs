8756e532885e1c9b0b9a5c475b47c3526b1d6720
Sergio Algorry <40408779+Sergio-Algorry@users.noreply.github.com>
2024-08-21 10:20:47 -0300

    Agregar archivos de proyecto.

---
+++ Calculadora.FE/Form1.Designer.cs
@@ -0,0 +1,147 @@
+﻿namespace Calculadora.FE
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtOper1 = new System.Windows.Forms.TextBox();
+            this.txtOper2 = new System.Windows.Forms.TextBox();
+            this.txtResultado = new System.Windows.Forms.TextBox();
+            this.btSumar = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtOper1
+            // 
+            this.txtOper1.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtOper1.Location = new System.Drawing.Point(26, 39);
+            this.txtOper1.Name = "txtOper1";
+            this.txtOper1.Size = new System.Drawing.Size(147, 32);
+            this.txtOper1.TabIndex = 0;
+            // 
+            // txtOper2
+            // 
+            this.txtOper2.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtOper2.Location = new System.Drawing.Point(225, 39);
+            this.txtOper2.Name = "txtOper2";
+            this.txtOper2.Size = new System.Drawing.Size(147, 32);
+            this.txtOper2.TabIndex = 1;
+            // 
+            // txtResultado
+            // 
+            this.txtResultado.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtResultado.Location = new System.Drawing.Point(26, 113);
+            this.txtResultado.Name = "txtResultado";
+            this.txtResultado.Size = new System.Drawing.Size(147, 32);
+            this.txtResultado.TabIndex = 2;
+            // 
+            // btSumar
+            // 
+            this.btSumar.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btSumar.Location = new System.Drawing.Point(179, 33);
+            this.btSumar.Name = "btSumar";
+            this.btSumar.Size = new System.Drawing.Size(40, 38);
+            this.btSumar.TabIndex = 3;
+            this.btSumar.Text = "+";
+            this.btSumar.UseVisualStyleBackColor = true;
+            this.btSumar.Click += new System.EventHandler(this.btSumar_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(23, 23);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(60, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Operador 1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(222, 23);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(54, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Oprador 2";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(23, 97);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(55, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Resultado";
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.ForeColor = System.Drawing.Color.Red;
+            this.button1.Location = new System.Drawing.Point(225, 106);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(131, 38);
+            this.button1.TabIndex = 7;
+            this.button1.Text = "Prueba";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(408, 164);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btSumar);
+            this.Controls.Add(this.txtResultado);
+            this.Controls.Add(this.txtOper2);
+            this.Controls.Add(this.txtOper1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtOper1;
+        private System.Windows.Forms.TextBox txtOper2;
+        private System.Windows.Forms.TextBox txtResultado;
+        private System.Windows.Forms.Button btSumar;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ Calculadora.FE/Form1.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Calculadora.FE
+{
+    public partial class Form1 : Form
+    {
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btSumar_Click(object sender, EventArgs e)
+        {
+            int Oper1;
+            int Oper2;
+            int Resultado;
+
+            Oper1 = Convert.ToInt32(txtOper1.Text);
+            Oper2 = Convert.ToInt32(txtOper2.Text);
+
+            Resultado = Oper1 + Oper2;
+
+            //txtResultado.Text = Convert.ToString(Resultado);
+            txtResultado.Text = Resultado.ToString();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int Oper1=3;
+            int Oper2=150;
+            int Resultado;
+
+            Resultado = Oper1 * Oper2;
+            txtResultado.Text = Resultado.ToString();
+        }
+    }
+}
